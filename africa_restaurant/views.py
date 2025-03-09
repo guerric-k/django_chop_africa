9130c7fa917b56701e60a3e995c581514a512e6f
guerric-k <160793947+guerric-k@users.noreply.github.com>
2025-03-09 23:22:44 +0000

    update view

--- africa_restaurant/views.py
+++ africa_restaurant/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello, world. You're at the africa restaurant index.")
+    return HttpResponse("Hello, world. You're at the africa restaurant home page.")
